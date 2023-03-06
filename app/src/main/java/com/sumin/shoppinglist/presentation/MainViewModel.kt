f3ceb03ee63e9a6e52f366ae265a7e0ea2a1e5b4
Nastya Koryakina <stayseena@mail.ru>
2023-03-06 15:11:11 +0300

    Initial commit

---
+++ app/src/main/java/com/sumin/shoppinglist/presentation/MainViewModel.kt
@@ -0,0 +1,30 @@
+package com.sumin.shoppinglist.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.sumin.shoppinglist.data.ShopListRepositoryImpl
+import com.sumin.shoppinglist.domain.DeleteShopItemUseCase
+import com.sumin.shoppinglist.domain.EditShopItemUseCase
+import com.sumin.shoppinglist.domain.GetShopListUseCase
+import com.sumin.shoppinglist.domain.ShopItem
+
+class MainViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = getShopListUseCase.getShopList()
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+    }
+
+    fun changeEnableState(shopItem: ShopItem) {
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+    }
+}
